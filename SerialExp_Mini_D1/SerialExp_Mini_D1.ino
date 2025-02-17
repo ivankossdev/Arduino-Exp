4adeb79dbf69d340afc2ba1cbccbf186572b8584
ivankossdev <ivan.koss@yandex.ru>
2025-02-17 10:59:50 +0300

    readString an clearSring

--- SerialExp_Mini_D1/SerialExp_Mini_D1.ino
+++ SerialExp_Mini_D1/SerialExp_Mini_D1.ino
@@ -1,30 +1,75 @@
-/* Prototip */ 
+#include <EEPROM.h>
+
+
+/* Prototip */
 void writeString();
+void readString();
+void clearString();
 
 /* Init */
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  EEPROM.begin(512);
 }
 
 /* Main*/
 void loop() {
   writeString();
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, LOW); 
-  delay(1);       
+  readString();
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1);
 }
 
 /* Func realistation */
-void writeString(){
+void writeString() {
   if (Serial.available() > 0) {
+    clearString();
     String str = Serial.readString();
     str.trim();
-    int i = 0; 
-    do{
-      Serial.printf("0x%x - %c\n\r", str[i], str[i]);
+    int i = 0;
+    do {
+      EEPROM.write(i, str[i]);
       i++;
-    }while(str[i] !='\0');
+    } while (str[i] != '\0');
+    if (EEPROM.commit()) {
+      Serial.println("EEPROM successfully committed");
+    } else {
+      Serial.println("ERROR! EEPROM commit failed");
+    }
   }
 }
+
+void readString(){
+  int i = 0;
+  char str[] = {'\0'};
+  do{
+    if(i > 512){
+      Serial.println("Error readString()");
+      i = 0;
+      break;
+    }
+    str[i] = (char)EEPROM.read(i);
+    i++;
+  }while(EEPROM.read(i) != 0);
+  str[i] = '\0';
+  Serial.println(str);
+
+}
+
+void clearString(){
+  for (int i = 0; i < 512; i++) { EEPROM.write(i, 0); }
+}
+
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+
+
+
+
+
+
+
+
+
