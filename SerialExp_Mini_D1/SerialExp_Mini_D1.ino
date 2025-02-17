b938a7ddfe580f3859aea26ec1933ee36ee734ab
ivankossdev <ivan.koss@yandex.ru>
2025-02-17 09:26:28 +0300

    func writeString

--- SerialExp_Mini_D1/SerialExp_Mini_D1.ino
+++ SerialExp_Mini_D1/SerialExp_Mini_D1.ino
@@ -1,20 +1,30 @@
+/* Prototip */ 
+void writeString();
 
-
+/* Init */
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Serial.println("\nStart programm");
 }
 
+/* Main*/
 void loop() {
-  
-  if (Serial.available() > 0) {
-    String str = Serial.readString();
-    str.trim();
-    Serial.println(str);
-  }
+  writeString();
   digitalWrite(LED_BUILTIN, HIGH); 
   delay(1000);                      
   digitalWrite(LED_BUILTIN, LOW); 
-  delay(10);       
+  delay(1);       
+}
+
+/* Func realistation */
+void writeString(){
+  if (Serial.available() > 0) {
+    String str = Serial.readString();
+    str.trim();
+    int i = 0; 
+    do{
+      Serial.printf("0x%x - %c\n\r", str[i], str[i]);
+      i++;
+    }while(str[i] !='\0');
+  }
 }
