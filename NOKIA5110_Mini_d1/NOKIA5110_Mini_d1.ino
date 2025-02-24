e60a97206eef50c82223d6321d6995e3ad8db41e
ivankossdev <ivan.koss@yandex.ru>
2025-02-24 10:28:44 +0300

    replace board

--- NOKIA5110_Mini_d1/NOKIA5110_Mini_d1.ino
+++ NOKIA5110_Mini_d1/NOKIA5110_Mini_d1.ino
@@ -20,8 +20,8 @@
 
 // *************** Defines ***************
 #define inverse  false
-#define contrast 0xBF // default is 0xBF set in LCDinit, Try 0xB1 <-> 0xBF, if your display is too dark/dim
-#define bias 0x12 // LCD bias mode 1:48: Try 0x12 or 0x13 or 0x14 usually
+#define contrast 0xbf // default is 0xBF set in LCDinit, Try 0xB1 <-> 0xBF, if your display is too dark/dim
+#define bias 0x13 // LCD bias mode 1:48: Try 0x12 or 0x13 or 0x14 usually
 
 // LCD Nokia 5110 pinout left to right
 // RST / CE / DC / DIN / CLK / VCC/ LIGHT / GND
