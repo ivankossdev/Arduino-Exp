9eaa71794deec1edfabf7b20eb219d31f89d6934
ivankossdev <ivan.koss@yandex.ru>
2025-03-18 11:01:48 +0300

    disp

--- NOKIA5110_BITMAP/NOKIA5110_BITMAP.ino
+++ NOKIA5110_BITMAP/NOKIA5110_BITMAP.ino
@@ -5,11 +5,11 @@
 #define bias 0x13
 
 /* Connection */
-#define RST D4 // 4-RST
-#define CE  D3 // 3-SCE
-#define DC  D5 // 5-D/C
-#define DIN D6 // 6-DN
-#define CLK D7 // 7-SCLK
+#define RST D4  // 4-RST
+#define CE D3   // 3-SCE
+#define DC D5   // 5-D/C
+#define DIN D6  // 6-DN
+#define CLK D7  // 7-SCLK
 static NOKIA5110_TEXT mylcd(RST, CE, DC, DIN, CLK);
 
 void setup() {
@@ -23,5 +23,14 @@ void loop() {
   mylcd.LCDCustomChar(epd_bitmap_, sizeof(epd_bitmap_) / sizeof(unsigned char), LCDPadding_None, true);
   mylcd.LCDgotoXY(9, 0);
   mylcd.printf("2025/03/17");
-  delay(1000);
+
+  int c0 = 0, c1 = 0;
+
+  for (int i = 0; i < 60; i++) {
+    c1 = i / 10 % 10;
+    c0 = i % 10;
+    mylcd.LCDgotoXY(9, 2);
+    mylcd.printf("count %d%d", c1, c0);
+    delay(1000);
+  }
 }
