67b38983f3032ddc829fba87a6086d7df41ac575
ivankossdev <ivan.koss@yandex.ru>
2025-03-20 13:02:12 +0300

    search i2c bus device

--- 8266_I2C_disp/8266_I2C_disp.ino
+++ 8266_I2C_disp/8266_I2C_disp.ino
@@ -1,12 +1,39 @@
 #include "display.h"
+#include <Wire.h>
+#include <cstring>
 
 void setup() {
   displayInit();
+  Wire.begin();
+}
+
+void clearMessage(char *message){
+  for(int i = 0; i < 512; i++)
+    message[i] = '\0';
 }
 
 void loop() {
-  NextRowString(true, (char *)"Test Display exapmle!!!");
-  delay(2500);
+  char message[512] = {'\0'};
+  sprintf(message, "Search I2C:\n");
+  NextRowString(false, message);
+  delay(2000);
   displayClear();
-  delay(1000);
+  int found = 0;
+
+  for(int i = 0; i < 127; i++){
+    Wire.beginTransmission(i);
+
+    int result = Wire.endTransmission();
+
+    if(result == 0){
+      sprintf(message, "Found 0%x\naddress", i);
+      NextRowString(false, message);
+      delay(2000);
+      clearMessage(message);
+      displayClear();
+      found++;
+    }
+  }
+  clearMessage(message);
+  delay(2000);
 }
