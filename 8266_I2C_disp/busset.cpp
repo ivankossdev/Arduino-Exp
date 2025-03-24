617ce2eb51980beaaec5eaaf148d27eb3c03a7a1
ivankossdev <ivan.koss@yandex.ru>
2025-03-24 15:47:29 +0300

    serial cmd set day

--- 8266_I2C_disp/busset.cpp
+++ 8266_I2C_disp/busset.cpp
@@ -1,7 +1,7 @@
 #include "busset.h"
-Busset::Busset(){
+Busset::Busset() {
   Serial.begin(115200);
-  Serial.printf("Start programm\n"); 
+  Serial.printf("Start programm\n");
 }
 
 void Busset::SetSerial() {
@@ -11,19 +11,23 @@ void Busset::SetSerial() {
       dsCmd.clear();
     dsCmd = Serial.readString();
   }
-  if (CheckCMD(dsCmd)) {
-    systime.SetTime(TimeDate::hr, TwoCharToInt(dsCmd[3], dsCmd[4]));
-    systime.SetTime(TimeDate::min, TwoCharToInt(dsCmd[5], dsCmd[6]));
-    systime.SetTime(TimeDate::sec, TwoCharToInt(dsCmd[7], dsCmd[8]));
+  if (CheckCMD(dsCmd) == SetCmd::time) {
+    systime.SetTime(TimeDate::hr, TwoCharToInt(dsCmd[4], dsCmd[5]));
+    systime.SetTime(TimeDate::min, TwoCharToInt(dsCmd[6], dsCmd[7]));
+    systime.SetTime(TimeDate::sec, TwoCharToInt(dsCmd[8], dsCmd[9]));
+  } else if (CheckCMD(dsCmd) == SetCmd::day) {
+    systime.SetDay(dsCmd[4] & 0x0f);
   }
 }
 
-bool Busset::CheckCMD(String cmd) {
+SetCmd Busset::CheckCMD(String cmd) {
   cmd.trim();
-  if (cmd[0] == 's' && cmd[1] == 'e' && cmd[2] == 't' && cmd.length() == 9)
-    return true;
-  else
-    return false;
+  if (cmd[0] == 's' && cmd[1] == 'e' && cmd[2] == 't' && cmd[3] == 't' && cmd.length() == 10) {
+    return SetCmd::time;
+  } else if (cmd[0] == 's' && cmd[1] == 'e' && cmd[2] == 't' && cmd[3] == 'd' && cmd.length() == 5) {
+    return SetCmd::day;
+  }
+  return SetCmd::nodata;
 }
 
 uint8_t Busset::TwoCharToInt(char c1, char c0) {
--- 8266_I2C_disp/busset.h
+++ 8266_I2C_disp/busset.h
@@ -4,13 +4,21 @@
 #include "ds3231.h"
 #define DS3231 0x68
 
+enum class SetCmd{
+  time,
+  day,
+  mounth,
+  year,
+  nodata
+};
+
 class Busset {
 public:
   Busset();
   void SetSerial();
 private:
   SystemTime systime{ DS3231 };
-  bool CheckCMD(String cmd);
+  SetCmd CheckCMD(String cmd);
   uint8_t TwoCharToInt(char c1, char c0);
 };
 
