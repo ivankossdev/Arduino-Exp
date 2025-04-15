188eccd08c67c4490be550d62e3ae3576903dcf5
ivankossdev <ivan.koss@yandex.ru>
2025-04-15 14:03:45 +0300

    test func shift

--- 8266_ND/8266_ND.ino
+++ 8266_ND/8266_ND.ino
@@ -1,7 +1,7 @@
 #include "display.h"
 #include "dsp_driver.h"
 
-unsigned char fig[8] = { 0xff, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xff };
+unsigned char fig[8] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
 DspDriver driver;
 
 void setup() {
@@ -10,41 +10,33 @@ void setup() {
 }
 
 void loop() {
-  driver.insertFig(fig, 8, 1, 0);
-  driver.insertFig(fig, 8, 10, 0);
-
-  driver.insertFig(fig, 8, 1, 8);
-  driver.insertFig(fig, 8, 10, 8);
+  for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 6; y++) {
+      driver.insertFig(fig, 8, x * 8, y * 8);
+    }
+  }
 
   mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-  delay(2000);
 
-  for (int i = 0; i < 9; i++) {
-    driver.shiftLeft(0, 8);
+  for (int y = 2; y < 7; y++) {
+    for (int x = 4; x < 12; x++) {
+      driver.insertFig(fig, 8, x * 5, y * 5);
+    }
     mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-    delay(100);
+    delay(50);
   }
   
-  for (int i = 0; i < 9; i++) {
-    driver.shiftLeft(1, 17);
+  delay(2000);
+
+  for (int i = 0; i < 80; i++) {
+    for (int i_ = 0; i_ < 6; i_++) {
+      driver.shiftLeft(i_, 80);
+    }
     mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-    delay(100);
+    delay(50);
   }
+
   delay(1000);
 
   driver.clearEx();
-}
-
-void test(unsigned char *img) {
-  for (int y = 2; y < 7; y++) {
-    for (int x = 4; x < 12; x++) {
-      driver.insertFig(img, 8, x * 5, y * 5);
-    }
-  }
-  mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-  delay(5000);
-
-  driver.clearEx();
-  mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-  delay(500);
 }
