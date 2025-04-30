d9eddae3a1126c46b5c8b0ecf93419d8f31b2a6d
ivankossdev <ivan.koss@yandex.ru>
2025-04-30 10:03:51 +0300

    add func ExShiftRigth

--- 8266_ND/8266_ND.ino
+++ 8266_ND/8266_ND.ino
@@ -2,7 +2,8 @@
 #include "dsp_driver.h"
 
 #define ARRAY 8
- unsigned char fig[ARRAY] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
+
+unsigned char fig[ARRAY] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
 
 DspDriver driver;
 
@@ -12,29 +13,26 @@ void setup() {
 }
 
 void loop() {
-  Example0();
+  ExShiftLift();
+  ExShiftRight();
 }
 
-void Example0() {
+void ExShiftLift() {
+
+  // Insert image
   for (int x = 0; x < 10; x++) {
     for (int y = 0; y < 6; y++) {
       driver.insertFig(fig, ARRAY, x * ARRAY, y * ARRAY);
     }
   }
 
+  // Show image
   mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-
-  for (int y = 2; y < 7; y++) {
-    for (int x = 4; x < 12; x++) {
-      driver.insertFig(fig, ARRAY, x * 5, y * 5);
-    }
-    mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-    delay(50);
-  }
 
   delay(2000);
 
-  for (int i = 0; i < 80; i++) {
+  // Left shift example
+  for (int i = 0; i < 40; i++) {
     for (int i_ = 0; i_ < 6; i_++) {
       driver.shiftLeft(i_, 80);
     }
@@ -45,4 +43,33 @@ void Example0() {
   delay(1000);
 
   driver.clearEx();
+}
+
+void ExShiftRight() {
+  // Insert image
+  for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 6; y++) {
+      driver.insertFig(fig, ARRAY, x * ARRAY, y * ARRAY);
+    }
+  }
+
+  // Show image
+  mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
+
+  delay(2000);
+
+  // Left shift example
+  // for (int i = 0; i < 80; i++) {
+  //   for (int i_ = 0; i_ < 6; i_++) {
+  //     driver.shiftLeft(i_, 80);
+  //   }
+  //   mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
+  //   delay(100);
+  // }
+
+  // delay(1000);
+
+  driver.clearEx();
+  mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
+  delay(1000);
 }
