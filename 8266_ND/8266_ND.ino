03b85f0229115a0e9d03fe605bf20fbd8e979d42
ivankossdev <ivan.koss@yandex.ru>
2025-04-30 09:50:18 +0300

    small refact

--- 8266_ND/8266_ND.ino
+++ 8266_ND/8266_ND.ino
@@ -1,7 +1,8 @@
 #include "display.h"
 #include "dsp_driver.h"
 
-unsigned char fig[8] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
+#define ARRAY 8
+ unsigned char fig[ARRAY] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
 
 DspDriver driver;
 
@@ -17,7 +18,7 @@ void loop() {
 void Example0() {
   for (int x = 0; x < 10; x++) {
     for (int y = 0; y < 6; y++) {
-      driver.insertFig(fig, 8, x * 8, y * 8);
+      driver.insertFig(fig, ARRAY, x * ARRAY, y * ARRAY);
     }
   }
 
@@ -25,7 +26,7 @@ void Example0() {
 
   for (int y = 2; y < 7; y++) {
     for (int x = 4; x < 12; x++) {
-      driver.insertFig(fig, 8, x * 5, y * 5);
+      driver.insertFig(fig, ARRAY, x * 5, y * 5);
     }
     mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
     delay(50);
@@ -38,7 +39,7 @@ void Example0() {
       driver.shiftLeft(i_, 80);
     }
     mylcd.LCDCustomChar(driver.ex, sizeof(driver.ex) / sizeof(unsigned char), LCDPadding_None, true);
-    delay(50);
+    delay(100);
   }
 
   delay(1000);
