7704ceb243117236a94ece07cd87438a97902d56
ivan.koss <ivan.koss@yandex.ru>
2025-07-13 21:26:10 +0300

    ref

--- soft_595/soft_595.ino
+++ soft_595/soft_595.ino
@@ -1,12 +1,12 @@
 /*
-  Arduino board >>    << 595 board
+  Arduino board >>    << 595 board default
             CLK  5    --> 11 Clock 
             STcp 6    --> 12 Storage Registet input 
             Data 7    --> 14 Data pin
 */
 #include "My595.h"
 
-MY595 my595;
+MY595 my595(14, 16, 10);
 void setup() {}
 
 void loop() {
