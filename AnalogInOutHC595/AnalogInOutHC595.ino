7e17365f798728f4a1bf249198b27eb6569e62de
ivankossdev <ivan.koss@yandex.ru>
2025-07-15 13:59:40 +0300

    new Example

--- AnalogInOutHC595/AnalogInOutHC595.ino
+++ AnalogInOutHC595/AnalogInOutHC595.ino
@@ -14,26 +14,54 @@ ESP8266
          SS   D4    12 | ST_CP | Storage Registet input
 
 Resistor connect
-      PIN  1 | GND  
-      PIN  2 | A0
-      PIN  3 | VCC +3V
+         PIN  1 | GND  
+         PIN  2 | A0
+         PIN  3 | VCC +3V
 */
 
 #define SS D4
 
 MYHW595 hw595(SS);
 
-void setup() {
-  Serial.begin(115200);
-  hw595.WriteByte(0x00);
+void ShiftLeft() {
+  for (uint8_t i = 0; i < 8; i++) {
+    int sensorValue = analogRead(A0);
+    hw595.WriteByte(1 << i);
+    delay(sensorValue / 4);
+  }
 }
 
-void loop() {
+void ShiftRight(){
+  for(int i = 7; i >= 0; i--){
+    int sensorValue = analogRead(A0);
+    hw595.WriteByte(1 << i);
+    delay(sensorValue / 4);
+  }
+}
+
+void Example_1(){
   int sensorValue = analogRead(A0);
   int res = sensorValue / 4;
   Serial.print("Analog value: ");
   Serial.println(res);
-  if(res >= 256) res = 255;
+  if(res >= 256) 
+    res = 255;
   hw595.WriteByte((char)res);
   delay(250);
+}
+
+void Example_2(){
+  
+  ShiftLeft();
+  ShiftRight();
+}
+
+void setup() {
+  // Serial.begin(115200);
+  hw595.WriteByte(0x00);
+}
+
+void loop() {
+  
+  Example_2();
 }
