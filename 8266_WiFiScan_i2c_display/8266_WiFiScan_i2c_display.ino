521de10b31ad03f27363676417878b88086fd959
ivankossdev <ivan.koss@yandex.ru>
2025-09-02 12:15:52 +0300

    delete old commetns

--- 8266_WiFiScan_i2c_display/8266_WiFiScan_i2c_display.ino
+++ 8266_WiFiScan_i2c_display/8266_WiFiScan_i2c_display.ino
@@ -8,7 +8,7 @@ void setup() {
   delay(10000);
 
   Serial.begin(115200);
-  Serial.println(F("\nESP8266 WiFi scan example"));
+  // Serial.println(F("\nESP8266 WiFi scan example"));
   // Set WiFi to station mode
   WiFi.mode(WIFI_STA);
   // Disconnect from an AP if it was previously connected
@@ -37,7 +37,6 @@ void scan() {
   bool hidden;
   int scanResult;
 
-  Serial.println(F("Starting WiFi scan..."));
   displayPrintText(&display, (char *)"Starting WiFi scan...");
 
   scanResult = WiFi.scanNetworks(/*async=*/false, /*hidden=*/true);
@@ -45,7 +44,6 @@ void scan() {
   if (scanResult == 0) {
     Serial.println(F("No networks found"));
   } else if (scanResult > 0) {
-    Serial.printf(PSTR("%d networks found:\n"), scanResult);
 
     displayClear(&display);
     sprintf(found, "%d networks found:", scanResult);
@@ -81,13 +79,12 @@ void scan() {
       }
       sprintf(found, "%02d:[CH %02d] %s", i, channel, ssid.c_str());
       displayPrintText(&display, found);
-      Serial.printf(PSTR("  %02d: [CH %02d] [%02X:%02X:%02X:%02X:%02X:%02X] %ddBm %c %c %-11s %3S %s\n"), i, channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5], rssi, (encryptionType == ENC_TYPE_NONE) ? ' ' : '*', hidden ? 'H' : 'V', phyMode.c_str(), wps, ssid.c_str());
+      
       yield();
     }
   } else {
     Serial.printf(PSTR("WiFi scan error %d"), scanResult);
   }
-  Serial.println(F("!!!END!!!\n"));
 
   // Wait a bit before scanning again
   delay(10000);
