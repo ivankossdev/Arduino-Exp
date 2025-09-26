f1b6b7cffeceb03f5c4d293e97781abec763876a
ivankossdev <ivan.koss@yandex.ru>
2025-09-26 13:34:15 +0300

    millis code

--- 8266_Serail_to_oled/8266_Serail_to_oled.ino
+++ 8266_Serail_to_oled/8266_Serail_to_oled.ino
@@ -6,16 +6,24 @@ bool readSerialPort();
 bool messageState = true;
 bool showMessage = true;
 
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0; 
+const long interval = 100;
+int ledState = LOW;
+
+
+
 void setup() {
   dsp.displayInit();
   dsp.displayPrintText((char*)"Init...");
   delay(1000);
   dsp.displayClear();
   dsp.displayPrintText((char*)"Test display\nPlese enter you \nmessage in terminal.");
+  pinMode(D5, OUTPUT);
 }
 
 void loop() {
-  if(showMessage){
+  if (showMessage) {
     delay(2000);
     Serial.println("\nEnter you message");
     showMessage = false;
@@ -23,10 +31,23 @@ void loop() {
 
   if (readSerialPort() && messageState) {
     dsp.displayClear();
+    dsp.displayPrintText((char*)"Serial message: ");
     dsp.displayPrintText(str);
     Serial.println(str);
     str = "";
     messageState = false;
+  }
+
+  currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(D5, ledState);
   }
 }
 
