456430067dd0ba89be7710a22d241b4041579ae4
ivankossdev <ivan.koss@yandex.ru>
2025-12-22 17:38:13 +0300

    char 1

--- Leo_ProMicro_LiquidDisp_CustomChar/Leo_ProMicro_LiquidDisp_CustomChar.ino
+++ Leo_ProMicro_LiquidDisp_CustomChar/Leo_ProMicro_LiquidDisp_CustomChar.ino
@@ -13,28 +13,38 @@ int lcdColumns = 16;
 int lcdRows = 2;
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+// 
+byte custChar_1_0[] = { B00001, B00001, B00001, B01111, B01111, B01111, B00001, B00001 };
+byte custChar_1_1[] = { B11000, B11000, B11000, B11000, B11000, B11000, B11000, B11000 };
+byte custChar_1_2[] = { B00001, B00001, B00001, B00001, B00001, B01111, B01111, B01111 };
+byte custChar_1_3[] = { B11000, B11000, B11000, B11000, B11000, B11111, B11111, B11111 };
 
-byte custChar_1[] = {
-  B11011,
-  B10101,
-  B10101,
-  B10001,
-  B10001,
-  B10101,
-  B10101,
-  B11011
-};
+byte custChar_2_0[] = {};
+byte custChar_2_1[] = {};
+byte custChar_2_2[] = {};
+byte custChar_2_3[] = {};
 
-byte custChar_2[] = {
-  B11011,
-  B10101,
-  B10001,
-  B10101,
-  B10001,
-  B10001,
-  B10101,
-  B11011
-};
+/*
+  1_0   1_1
+  00011 11000
+  00011 11000
+  11100 00111
+  11100 00111
+  11100 00111
+  00000 01110
+  00000 11100
+  00001 11000
+  1_2     1_3   
+  00011 10000
+  00111 00000
+  01110 00000
+  11100 00000
+  11100 00000
+  11111 11111
+  11111 11111
+  11111 11111
+
+*/
 
 
 void setup() {
@@ -42,23 +52,26 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
-  lcd.createChar(0, custChar_1);
-  lcd.createChar(1, custChar_2);
+  lcd.createChar(0, custChar_1_0);
+  lcd.createChar(1, custChar_1_1);
+  lcd.createChar(2, custChar_1_2);
+  lcd.createChar(3, custChar_1_3);
   lcd.home();
 }
 
 
 void loop() {
-  for (int i = 0; i < 16; i++) {
-    lcd.setCursor(i, 0);
-    lcd.printByte(0);
-    lcd.setCursor(i, 1);
-    lcd.printByte(1);
-    delay(100);
-  }
-
-
-  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.printByte(0);
+  lcd.setCursor(1, 0);
+  lcd.printByte(1);
+  lcd.setCursor(0, 1);
+  lcd.printByte(2);
+  lcd.setCursor(1, 1);
+  lcd.printByte(3);
+  lcd.setCursor(2, 0);
+  lcd.print("2345678901");
+  delay(10000);
   lcd.clear();
   delay(1000);
 }
