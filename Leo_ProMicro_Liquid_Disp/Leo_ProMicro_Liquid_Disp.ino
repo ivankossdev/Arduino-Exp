bdc7491054438a4b141a11d1ee5757a2d76633c4
ivankossdev <ivan.koss@yandex.ru>
2025-12-22 09:47:45 +0300

    del

--- Leo_ProMicro_Liquid_Disp/Leo_ProMicro_Liquid_Disp.ino
+++ Leo_ProMicro_Liquid_Disp/Leo_ProMicro_Liquid_Disp.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "I2C_Device.h"
-#include <BH1750.h>
 
 /* 
   Leonardo pro micro
@@ -16,37 +15,24 @@ String devices = "";
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 I2C_Device search;
 String messageToScroll = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-BH1750 lightMeter(0x23);
 
 void setup() {
-  Wire.begin();
-  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
-    Serial.println(F("BH1750 Advanced begin"));
-  } else {
-    Serial.println(F("Error initialising BH1750"));
-  }
+
   lcd.init();
   lcd.backlight();
   devices = search.searchDevice();
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Display test");
-  scrollText(1, devices, 350, lcdColumns);
+  scrollText(1, devices, 300, lcdColumns);
   lcd.clear();
 }
 
 
 void loop() {
   lcd.setCursor(0, 0);
-  lcd.print("Luxmetr: ");
-  if (lightMeter.measurementReady()) {
-    float lux = lightMeter.readLightLevel();
-    lcd.setCursor(0, 1);
-    lcd.print(lux);
-  }
-
-  // lcd.print("Scroll string");
-  // scrollText(1, messageToScroll, 350, lcdColumns);
+  lcd.print("Scroll string");
+  scrollText(1, messageToScroll, 350, lcdColumns);
 }
 
 void scrollText(int row, String message, int delayTime, int lcdColumns) {
