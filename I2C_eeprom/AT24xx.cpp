cdef7ecb205ac8edf2355c7963445282d2f0a125
ivan.koss <ivan.koss@yandex.ru>
2026-01-08 01:46:48 +0300

    class At24

---
+++ I2C_eeprom/AT24xx.cpp
@@ -0,0 +1,28 @@
+#include "AT24xx.h"
+
+
+At24::At24(uint8_t address_){
+  this->address = address_;
+  Wire.begin();
+}
+
+void At24::writeByte(unsigned int memAddress, byte data) {
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  Wire.write((memAddress >> 8) & 0xFF);  // High byte of address
+  Wire.write(memAddress & 0xFF);         // Low byte of address
+  Wire.write(data);                      // Data byte
+  Wire.endTransmission();
+  delay(5);  
+}
+
+byte At24::readByte(unsigned int memAddress) {
+  byte data = 0;
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  Wire.write((memAddress >> 8) & 0xFF);
+  Wire.write(memAddress & 0xFF);
+  Wire.endTransmission();
+
+  Wire.requestFrom(EEPROM_I2C_ADDRESS, 1);
+  if (Wire.available()) data = Wire.read();
+  return data;
+}
