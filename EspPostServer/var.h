ba8c3e32df30d74ca952aec97eb37ae57c4df56a
ivankossdev <ivan.koss@yandex.ru>
2026-01-15 14:33:42 +0300

    WiFi-AP post

--- EspPostServer/var.h
+++ EspPostServer/var.h
@@ -1,13 +1,4 @@
-/* Create a WiFi access point and provide a web server on it. */
-
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-
-const char *ssid = "ESP-AP";
-const char *password = "12345678";
-
-ESP8266WebServer server(80);
+#ifndef _VAR_H_
 
 const String postForms = "<!DOCTYPE html>\n\
 <html lang=\"en\">\n\
@@ -48,59 +39,4 @@ const String postForms = "<!DOCTYPE html>\n\
 </body>\n\
 </html>\n";
 
-void handleRoot() {
-  server.send(200, "text/html", postForms);
-}
-
-void handleForm() {
-  if (server.method() != HTTP_POST) {
-    server.send(405, "text/plain", "Method Not Allowed");
-  } else {
-    String message = "POST form was:\n";
-    for (uint8_t i = 0; i < server.args(); i++) {
-      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-    }
-    Serial.println(message);
-    server.send(200, "text/plain", message);
-  }
-}
-
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) { message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; }
-  server.send(404, "text/plain", message);
-}
-
-void setup() {
-  delay(1000);
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Configuring access point...\n");
-  
-  IPAddress ip(192,168,0,100);     
-  IPAddress gateway(192,168,0,100);   
-  IPAddress subnet(255,255,255,0);
-
-  WiFi.softAPConfig(ip, gateway, subnet);
-  WiFi.softAP(ssid, password, 5);
-
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  server.on("/", handleRoot);
-  server.on("/postform/", handleForm);
-   server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-void loop() {
-  server.handleClient();
-}
+#endif 
--- WiFi_AP_post/WiFi_AP_post.ino
+++ WiFi_AP_post/WiFi_AP_post.ino
@@ -1,46 +1,31 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include "var.h"
 
-#ifndef STASSID
-#define STASSID "Beeline_2G_FF3761"
-#define STAPSK "qJaHxeUC"
-#endif
-
-const char* ssid = STASSID;
-const char* password = STAPSK;
+const char *ssid = "ESP-AP";
+const char *password = "12345678";
 
 ESP8266WebServer server(80);
 
-const int led = LED_BUILTIN;
-
 void handleRoot() {
-  digitalWrite(led, 1);
   server.send(200, "text/html", postForms);
-  digitalWrite(led, 0);
 }
 
 void handleForm() {
   if (server.method() != HTTP_POST) {
-    digitalWrite(led, 1);
     server.send(405, "text/plain", "Method Not Allowed");
-    digitalWrite(led, 0);
   } else {
-    digitalWrite(led, 1);
     String message = "POST form was:\n";
     for (uint8_t i = 0; i < server.args(); i++) {
       message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }
     Serial.println(message);
     server.send(200, "text/plain", message);
-    digitalWrite(led, 0);
   }
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -51,39 +36,31 @@ void handleNotFound() {
   message += "\n";
   for (uint8_t i = 0; i < server.args(); i++) { message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; }
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
 }
 
-void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+void setup() {
+  delay(1000);
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  Serial.println();
+  Serial.print("Configuring access point...\n");
+  
+  IPAddress ip(192,168,0,100);     
+  IPAddress gateway(192,168,0,100);   
+  IPAddress subnet(255,255,255,0);
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid, password, 5);
 
-  if (MDNS.begin("esp8266")) { Serial.println("MDNS responder started"); }
-
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
   server.on("/", handleRoot);
-
   server.on("/postform/", handleForm);
-
-  server.onNotFound(handleNotFound);
-
+   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+void loop() {
   server.handleClient();
 }
