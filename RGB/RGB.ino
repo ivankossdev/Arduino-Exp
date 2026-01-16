ce6ea1187576afb046216888cf3e7a66a056746d
ivankossdev <ivan.koss@yandex.ru>
2026-01-16 17:04:55 +0300

    RGB example

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,28 +1,31 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
-#define NUMPIXELS 8
-#define DELAYVAL 1000  // Time (in milliseconds) to pause between pixels
+#define NUM_LEDS 8
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ring(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pixels.begin();
-  pixels.clear();
-  pixels.show();
+  ring.begin();
+  ring.show();
 }
 
 void loop() {
+  rainbowBreathing(10);
+}
 
-  for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
-    pixels.setPixelColor(i, pixels.Color(i + 2, i + 1, 0));
-    pixels.show();
-    delay(10);
+void rainbowBreathing(uint8_t wait) {
+  static uint16_t hueBase = 0;
+  hueBase += 20;  // rainbow shift
+
+  // Smooth breathing brightness: 0 → 255 → 0
+  float breathe = (exp(sin(millis() / 1000.0 * PI)) - 0.36787944) * 108.0;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    uint16_t hue = hueBase + (i * 65536L / NUM_LEDS);
+    uint32_t color = ring.ColorHSV(hue, 255, (uint8_t)breathe);
+    ring.setPixelColor(i, ring.gamma32(color));
   }
 
-  delay(DELAYVAL);
-  pixels.clear();
-  pixels.show();
-  delay(DELAYVAL);
-  pixels.clear();
-  pixels.show();
+  ring.show();
+  delay(wait);
 }
