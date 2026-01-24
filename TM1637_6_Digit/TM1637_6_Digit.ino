f98457af7f7966ff1d6202fbcfb407173bef55a8
ivan.koss <ivan.koss@yandex.ru>
2026-01-24 09:51:10 +0300

    counter

--- TM1637_6_Digit/TM1637_6_Digit.ino
+++ TM1637_6_Digit/TM1637_6_Digit.ino
@@ -4,24 +4,38 @@
 #define CLK 2
 #define DIO 3
 
-#define TEST_DELAY   2000
-uint8_t data[6] = {'\0'};
-
+uint8_t data[6] = { '\0' };
 
 TM1637Display display(CLK, DIO);
 
-void setup()
-{
+void setup() {
   display.setBrightness(0x0);
 }
 
-void loop()
-{
-  data[0] = display.encodeDigit(2);
-  data[1] = display.encodeDigit(1);
-  data[2] = display.encodeDigit(0);
-  data[3] = display.encodeDigit(5);
-  data[4] = display.encodeDigit(4);
-  data[5] = display.encodeDigit(3);
+void loop() {
+  for (int i = 0; i < 60; i++) {
+    prntSec(i);
+    delay(250);
+  }
+}
+
+void prntSec(unsigned int digital) {
+  int count = 2;
+  int x = 1;
+  do {
+    count--;
+    int d = digital / x % 10;
+    switch (count) {
+      case 0: data[4] = display.encodeDigit(d); break;
+      case 1: data[3] = display.encodeDigit(d); break;
+    }
+    x *= 10;
+  } while (digital / x);
+
+  data[2] = 0;
+  data[1] = 0 | 0b10000000;
+  data[0] = 0;
+  data[5] = 0 | 0b10000000;
+
   display.setSegments(data, 6);
 }
