28ae80f303ce76881456b6078fd7a37e99e904d1
ivan.koss <ivan.koss@yandex.ru>
2026-02-08 03:26:32 +0300

    pwm

--- SerialReadWrite/SerialReadWrite.ino
+++ SerialReadWrite/SerialReadWrite.ino
@@ -2,6 +2,7 @@
 #include "serialSet.h"
 
 SerialSet serialSet(9600);
+int ledPin = 9;
 
 void setup() {
   serialSet.Init();
@@ -9,8 +10,17 @@ void setup() {
   digitalWrite(5, LOW);
   pinMode(6, OUTPUT);
   digitalWrite(6, LOW);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   serialSet.SetCMD();
+  for (int brightness = 0; brightness <= 255; brightness++) {
+    analogWrite(ledPin, brightness);
+    delay(10);
+  }
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(ledPin, brightness);
+    delay(10);
+  }
 }
